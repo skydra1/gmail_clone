9c2edea2843c46bcd3529d91674c61f6455e3767
skydra1 <108009982+skydra1@users.noreply.github.com>
2024-11-26 00:27:34 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -8,7 +8,7 @@ const Connection = () => {
     const DB_URI = process.env.DB_URL;
     try {
         mongoose.connect(DB_URI, { useNewUrlParser: true });
-        mongoose.set('strictQuery', false);
+      
         console.log('Database connected sucessfully');
     } catch (error) {
         console.log('Error while connecting with the database ', error.message)
