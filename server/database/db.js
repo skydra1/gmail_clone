d8335600061c014b848439022a08c9a160417ba9
skydra1 <108009982+skydra1@users.noreply.github.com>
2024-11-26 00:26:16 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -3,11 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const USERNAME = process.env.DB_USERNAME;
-const PASSWORD = process.env.DB_PASSWORD; 
 
 const Connection = () => {
-    const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-8j16moz-shard-00-00.zwycdvh.mongodb.net:27017,ac-8j16moz-shard-00-01.zwycdvh.mongodb.net:27017,ac-8j16moz-shard-00-02.zwycdvh.mongodb.net:27017/?ssl=true&replicaSet=atlas-usw39h-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const DB_URI = process.env.DB_URL;
     try {
         mongoose.connect(DB_URI, { useNewUrlParser: true });
         mongoose.set('strictQuery', false);
